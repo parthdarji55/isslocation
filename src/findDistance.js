226e444c060ef407d5bbd7e72c2fbc3fe652a40b
parthdarji55 <60765475+parthdarji55@users.noreply.github.com>
2020-05-04 19:06:20 -0400

    Update findDistance.js

--- src/findDistance.js
+++ src/findDistance.js
@@ -6,7 +6,7 @@ function FindDistance(issLat, issLong, yourLocationLat, yourLocationLong) {
     //3963 is the radius of the Earth in Miles
     let distance = 3963.0 * Math.acos(Math.sin(issLat)*Math.sin(yourLocationLat)+Math.cos(issLat)*Math.cos(yourLocationLat)*Math.cos(yourLocationLong-issLong))
  distance = Math.round(distance)
- console.log("hi")
+
   return (
 distance
   );
